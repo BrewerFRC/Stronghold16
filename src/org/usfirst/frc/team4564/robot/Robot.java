cb6d756c9da3037139f27583cbfcc11cd5fc6182
Qwertyness <qwertynessthedev@gmail.com>
2016-01-08 07:13:58 -0500

    Project template.

---
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -0,0 +1,22 @@
+
+package org.usfirst.frc.team4564.robot;
+
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+    public Robot() {
+        
+    }
+    
+    public void autonomous() {
+        
+    }
+    
+    public void operatorControl() {
+        
+    }
+    
+    public void test() {
+    }
+}
