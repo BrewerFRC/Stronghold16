bc49b1019d9f0d669a1943c30a92b0a98f5eb810
Qwertyness <qwertynessthedev@gmail.com>
2016-01-19 18:15:48 -0500

    Added new methods to Common.

--- src/org/usfirst/frc/team4564/robot/Common.java
+++ src/org/usfirst/frc/team4564/robot/Common.java
@@ -5,16 +5,16 @@ public class Common {
 
 	public static void debug(String a) {
 		System.out.println(System.currentTimeMillis()+": " + a);
-		
 	}
 	
-	public static void dashStr(String a) {
-		SmartDashboard.putString(": ",a);
-		
+	public static void dashStr(String title, String a) {
+		SmartDashboard.putString(title + ": ",a);
 	}
 	
-	public static void dashNum(int a) {
-		SmartDashboard.putNumber(": ", a);
-		
+	public static void dashNum(String title, int a) {
+		SmartDashboard.putNumber(title + ": ", a);
+	}
+	public static void dashNum(String title, double a) {
+		SmartDashboard.putNumber(title + ": ", a);
 	}
 }
