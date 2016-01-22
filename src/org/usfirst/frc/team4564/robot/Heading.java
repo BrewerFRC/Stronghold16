9b76ce62742a11cfa926b2b0679e45deb19e67d1
Qwertyness <qwertynessthedev@gmail.com>
2016-01-21 19:22:00 -0500

    Fixed PID target handling.

--- src/org/usfirst/frc/team4564/robot/Heading.java
+++ src/org/usfirst/frc/team4564/robot/Heading.java
@@ -6,14 +6,12 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 public class Heading {
 	private Gyro gyro;
 	private PID pid;
-	private double targetHeading;
 	private boolean headingHold;
 	
 	public Heading(int pin, double p, double i, double d, double sensitivity) {
 		pid = new PID(p, i, d, true);
 		//PID is dealing with error; an error of 0 is always desired.
 		pid.setTarget(0.0);
-		targetHeading = 0.0;
 		gyro = new AnalogGyro(pin);
 	}
 	
@@ -39,6 +37,15 @@ public class Heading {
 		return (gyro.getAngle()*100) % 360 / 100;
 	}
 	
+	public void setHeading(double heading) {
+		//Find the short and long path to the desired heading.
+		double changeLeft = (-360 + (heading - getHeading())) % 360;
+		double changeRight = (360 - (getHeading() - heading)) % 360;
+		double change = (Math.abs(changeLeft) < Math.abs(changeRight)) ? changeLeft : changeRight;
+		
+		pid.setTarget(gyro.getAngle() + change);
+	}
+	
 	public void headingHold() {
 		if (headingHold) {
 			reset();
@@ -48,21 +55,17 @@ public class Heading {
 			resetPID();
 			headingHold = true;
 			//Set target angle to current heading.
-			targetHeading = getHeading();
+			setHeading(getHeading());
 		}
 	}
 	public boolean isHeadingHold() {
 		return headingHold;
 	}
 	
-	public double turnRate(double input) {
+	public double turnRate() {
 		if (headingHold) {
-			//Find the short and long path to the desired heading.
-			double changeLeft = (-360 + (targetHeading - input)) % 360;
-			double changeRight = (360 - (input - targetHeading)) % 360;
-			
 			//Return the PID calculation of the shorter path.
-			return pid.calc((changeLeft < changeRight) ? changeLeft : changeRight);
+			return pid.calc(gyro.getAngle());
 		}
 		else {
 			return 0.0;
