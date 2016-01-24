a1269e1b011a5a7bc50945a4b36a326e1aa9efe7
FRC4564 <paedagogs@myfairpoint.net>
2016-01-24 12:52:17 -0500

    Implemented Heading into DriveTrain.

--- src/org/usfirst/frc/team4564/robot/Constants.java
+++ src/org/usfirst/frc/team4564/robot/Constants.java
@@ -13,6 +13,16 @@ public class Constants {
 	public static final int DIO_DRIVE_FR_ENCODER_A = 0;
 	public static final int DIO_DRIVE_FR_ENCODER_B = 1;
 	
+	//Analog Ports
+	public static final int ANA_GYRO = 0;
+	
+	//Gyro constants
+	public static final double GYRO_P = 0.03;
+	public static final double GYRO_I = 0.0;
+	public static final double GYRO_D = 0.0;
+	public static final double GYRO_SENSITIVITY = 0.00669;
+	
+	
 	//Robot loop speed
 	public static final double REFRESH_RATE = 50;
 
