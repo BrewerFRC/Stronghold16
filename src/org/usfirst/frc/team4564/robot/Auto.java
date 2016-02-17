96d328a4386e43750f71b6572b557a0c7820cc54
Jacob4564 <stevejacobs@iname.com>
2016-02-16 22:25:41 -0500

    Worked on auto class

--- src/org/usfirst/frc/team4564/robot/Auto.java
+++ src/org/usfirst/frc/team4564/robot/Auto.java
@@ -5,7 +5,6 @@ public class Auto {
 	DriveTrain dt;
 	Bat bat;
 	Thrower t;
-	public Shield shield;
 	
 	//Shield detection states for driving through a defense
 	public static final int AUTO_INIT = 0;						// Auto mode has to be initialed
@@ -14,56 +13,63 @@ public class Auto {
 	public static final int DETECT_EXIT = 3;					// Within the defense, awaiting exit from Shield
 	public static final int EXIT_WAIT = 4;						// Verify shield exit for a set amount of time
 	public static final int DEFENSE_CLEARED = 5;				// Verified that defense has been cleared
+	
 	//Shield detection states for returning through Portcullis or Sally Port
 	public static final int DETECT_RETURN = 10;					// Awaiting for initial Shield detect
 	public static final int RETURN_WAIT = 11;					// Verify shield detect for a set amount of time
 	public static final int IN_DEFENSE = 12;					// Robots within the defense
-	//driveState constants.
-	public static final int NOT_DRIVING = 0;
-	public static final int DRIVING = 1;
-	public static final int DEFENSE_CROSSED = 2;
-	public static final int TURNING = 3;
-	public static final int TURN_COMPLETE = 4;
-	public static final int MOVING_TO_TARGET = 5;
-	public static final int REACHED_TARGET = 6;
-	public static final int MOVING_TO_OPEN = 7;
-	public static final int OPENING_DEFENSE = 8;
-	public static final int ALIGNMENT_COMPLETE = 9;
-	public static final int FIRING = 10;
-	public static final int FINI = 11;
+	
+	//Shield related constants
+	public static final double MAX_SHIELD_DISTANCE = 15;
+	public static final double VERIFICATION_TIME = 300;		//Amount of necessary time to verify shield detect
+
+	//driveDefense states.
+	public static final int NOT_DRIVING = 0;					//Ready to start
+	public static final int DRIVING = 1;						//Robot moving, waiting to clear Shield
+	public static final int DEFENSE_CROSSED = 2;				//Robot cleared shield and now stopped.
+
+	//Autorun states
+	private static final int AUTO_SETUP = 1;					//Prep start of autorun
+	private static final int AUTO_DRIVE = 2;					//Drive through the defense, if one is selected
+	private static final int AUTO_NEXT_ACTION = 3;				//Determine what is the next action; UTURN, SHOOT, or STOP
+	private static final int AUTO_UTURN = 4;					//Drive to the return defense, if one is specifed
+	private static final int AUTO_SHOOT = 5;					//Drive to tower and shoot
+	private static final int AUTO_STOP = 6;						//Autorun complete; robot stopped.
+	
+	//Autorun parameters loaded from networktables
+	private int paramStartingPlatform;
+	private int paramDefenseType;	
+	private int paramSelectedAction;
+	private int paramTargetPlatform;
+	private int paramTarget;
+	//Action parameter constants
+	private static final int ACTION_STOP = 0;
+	private static final int ACTION_UTURN = 1;
+	private static final int ACTION_SHOOT = 2;
+	//Defense parameter constants
+	private static final int DEFENSE_LOWBAR = 1;
+	private static final int DEFENSE_ROCK_WALL = 2;
+	private static final int DEFENSE_MOAT = 3;
+	private static final int DEFENSE_PORTCULLIS = 3;
+
+	//State variables
+	private int shieldState = AUTO_INIT;
+	private int driveState = NOT_DRIVING;
+	private int autorunState = AUTO_SETUP;
+
 	//Field Dimension constants.
 	public static final double PLATFORM_WIDTH = 52.5;
 	public static final double ABSOLUTE_CASTLE_X = 170.6113;
-	//Auto action constants.
-	public static final double AUTO_HIGHGOAL_SPEED = 100;
-	//driveDefense variables
-	public int defenseType;
-	public int selectedAction;
-	public int driveState = 0;
-	public int targetPlatform;
-	public int target;
-	public int startingPlatform;
 
-	
-	
-	
-	
-	
-	//public static final double MIN_SHIELD_DISTANCE = 0;
-	public static final double MAX_SHIELD_DISTANCE = 15;
-	public static final double VERIFICATION_TIME = 300;		//Amount of necessary time to verify panel detect
-	
-	//updateAuto variables
-	public int shieldState = AUTO_INIT;
+	//Misc variables
 	public long detectTime;		//Start time of Sheild detect
-	//public long currentTime;
 	public double shieldDistance;  //The ultrasonic sensor distance upon exit of a defense
+	
 	
 	//Auto constructor
 	public Auto(DriveTrain dt, Bat bat) {
 		this.dt = dt;
 		this.bat = bat;
-		shield = new Shield();
 		dt.setPIDDrive(true);
 	}
 	
@@ -75,16 +81,16 @@ public class Auto {
         dt.setSafetyEnabled(false);
     	dt.heading.setHeadingHold(true);
     	// Get initial auto parameters from network tables.  They'll default to zero if not specified.
-    	shield.startingPlatform = (int) Robot.table.getNumber("platform", 0);
-    	shield.targetPlatform = (int) Robot.table.getNumber("targetPlatform", 0);
-    	shield.defenseType = (int) Robot.table.getNumber("defense", 0);
-    	shield.selectedAction = (int) Robot.table.getNumber("action", 0);
+    	paramStartingPlatform = (int) Robot.table.getNumber("platform", 0);
+    	paramDefenseType = (int) Robot.table.getNumber("defense", 0);
+    	paramTargetPlatform = (int) Robot.table.getNumber("targetPlatform", 0);
+    	paramSelectedAction = (int) Robot.table.getNumber("action", 0);
 	}
 	
 	// Using the ultrasonic sensor, this logic determines when the robot passes through a shield.
-	// The value of "shieldDistance" will give the distance that the robot is away from the shield.
+	// The value of "shieldDistance" will give the distance that the robot was away from the shield just before exit.
 	// Returns true when defense is cleared. 
-	public boolean shieldCross () {
+	public boolean shieldCrossed() {
 		boolean isCleared = false;
 		switch (shieldState) {
 
@@ -141,9 +147,8 @@ public class Auto {
 	// Retruns true when within a shield.
 	public boolean shieldReturn() {
 		boolean inDefense = false;
-		
 		switch (shieldState) {
-		
+
 			case DETECT_RETURN:
 				if (bat.getDistance() <= MAX_SHIELD_DISTANCE) { 
 					detectTime = Common.time();
@@ -168,15 +173,97 @@ public class Auto {
 		return inDefense;
 	}
 
+	// Given a defenseType, this method will drive the robot through a defense.
+    // It will return true when the defense is cleared.
+	public boolean driveDefense(int defenseType) {
+		
+		boolean defenseCleared = false;
+		shieldState = AUTO_INIT;  //set initial shield state
 	
-		
+		switch(defenseType) {
+			//LOW-BAR
+			case DEFENSE_LOWBAR:  
+				switch(driveState) {
 
-		
-		//Execute Auto based upon startingPlatform, defenseType, and selectedAction.
-		public void driveDefense(int defenseType) {
-		
-			switch(defenseType) {
-				case 0:
+					case NOT_DRIVING:
+						driveState = DRIVING;
+						dt.setDrive(0.5, 0.0);
+						break;
+					
+					case DRIVING:
+						if (shieldCrossed()) {
+							dt.setDrive(0.0, 0.0);
+							driveState = DEFENSE_CROSSED;
+						}
+						break;
+					
+					case DEFENSE_CROSSED:
+						defenseCleared = true;
+						break;
+				}
+				
+			//PORTCULLIS
+			case DEFENSE_PORTCULLIS:
+				switch(driveState) {
+				
+				case NOT_DRIVING:
+					break;
+				
+				case DRIVING:
+					break;
+
+				case DEFENSE_CROSSED:
+					defenseCleared = true;
+					break;
+					
+				}
+		}
+		return defenseCleared;
+	}
+				
+					
+	//Execute an full auto routine based on the user provided parameters.
+	public void autorun() {
+		driveState = NOT_DRIVING;  // set initial drive state
+
+		switch (autorunState) {
+			case AUTO_SETUP:
+				if (paramStartingPlatform > 0 && paramDefenseType > 0) {
+					autorunState = AUTO_DRIVE;
+				}
+				break;
+			case AUTO_DRIVE:
+				if (driveDefense(paramDefenseType)) {
+					autorunState = AUTO_NEXT_ACTION;
+				}	
+				break;
+			case AUTO_NEXT_ACTION:
+				switch (paramSelectedAction) {
+				case ACTION_STOP:
+					autorunState = AUTO_STOP;
+					break;
+				case ACTION_UTURN:
+					autorunState = AUTO_UTURN;
+					break;
+				case ACTION_SHOOT:
+					autorunState = AUTO_SHOOT;
+					break;
+				}
+				break;
+			case AUTO_UTURN:
+				
+				break;
+			case AUTO_SHOOT:
+				
+				break;
+			case AUTO_STOP:
+				
+				break;
+		}
+	}
+				
+					
+/*					
 					if (driveState < DEFENSE_CROSSED) {
 						if (driveState == NOT_DRIVING) {
 							dt.setDrive(.5,0);
@@ -245,52 +332,35 @@ public class Auto {
 			
 		}
 	}
-
+*/
 		
-		//Determines right or left turn based on starting position and target.
-		public double turnLogic(double target, int startingPlatform) {
-			if ( target - startingPlatform > 0) {
-				return 90;
-			} else {
-				return -90;
-			}
-		}
-		
-		//Find absolute robot position on field after crossing intial defense.
-		public double xAbs(int startingPlatform, double PLATFORM_WIDTH, double shieldDistance) {
-			return (startingPlatform * PLATFORM_WIDTH - shieldDistance);
-		}
-		
-		//Drive to selected platform based on starting position.
-		public double xPlatformTarget(int targetPlatform, double PLATFORM_WIDTH, double shieldDistance) {
-			return (targetPlatform * PLATFORM_WIDTH - (PLATFORM_WIDTH * .5));
-		}
-		
-		//Drive to castle based on starting position.
-		public double xCastleTarget(double ABSOLUTE_CASTLE_X, double xABS){
-			return (Math.abs(xABS - ABSOLUTE_CASTLE_X));
-		}
-		
-		//Drive to target on field based on current position.
-		public double xDrive (double xAbs, double xTarget) {
-			return (xAbs - xTarget);
-		}
-		
-		//Have we crossed the initial defense?
-		public boolean defenseComplete() {
-			if (shieldState == DETECT_RETURN) {
-				return (true);
-			} else { 
-				return (false);
-			}
-		}
-		// Have we crossed the defense when returning?
-		public boolean returnDefenseComplete() {
-			if (shieldState == CROSSED_RETURN_DEFENSE) {
-				return (true);
-			} else {
-				return (false);
-			}
+	//Determines right or left turn based on starting position and target.
+	public double turnLogic(double target, int startingPlatform) {
+		if ( target - startingPlatform > 0) {
+			return 90;
+		} else {
+			return -90;
 		}
 	}
+	
+	//Find absolute robot position on field after crossing intial defense.
+	public double xAbs(int startingPlatform, double PLATFORM_WIDTH, double shieldDistance) {
+		return (startingPlatform * PLATFORM_WIDTH - shieldDistance);
+	}
+	
+	//Drive to selected platform based on starting position.
+	public double xPlatformTarget(int targetPlatform, double PLATFORM_WIDTH, double shieldDistance) {
+		return (targetPlatform * PLATFORM_WIDTH - (PLATFORM_WIDTH * .5));
+	}
+	
+	//Drive to castle based on starting position.
+	public double xCastleTarget(double ABSOLUTE_CASTLE_X, double xABS){
+		return (Math.abs(xABS - ABSOLUTE_CASTLE_X));
+	}
+	
+	//Drive to target on field based on current position.
+	public double xDrive (double xAbs, double xTarget) {
+		return (xAbs - xTarget);
+	}
+	
 }
