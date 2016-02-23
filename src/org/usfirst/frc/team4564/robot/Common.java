a4b399bdbe1b4c5f10cb31a7d2c96a470e5b9305
Qwertyness <qwertynessthedev@gmail.com>
2016-02-22 20:02:44 -0500

    Improved timestamp for common debug.

--- src/org/usfirst/frc/team4564/robot/Common.java
+++ src/org/usfirst/frc/team4564/robot/Common.java
@@ -1,12 +1,16 @@
 package org.usfirst.frc.team4564.robot;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Common {
+	private static final DateFormat formatter = new SimpleDateFormat("HH:mm:ss:SSS");
 
 	public static void debug(String text) {
-		System.out.println(System.currentTimeMillis() + text);
+		System.out.println(formatter.format(new Date(time())) + text);
 	}
 	
 	public static void dashStr(String title, String a) {
